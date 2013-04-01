11d3f5d826771d327385382261230e9c44252480
John Engelhart <johne@syntellia.com>
2013-04-01 11:43:59 -0700

    Add some automated TestFlight abilities to the build logic.
    This is a first pass at adding some automated TestFlight stuff to the build logic.
    
    This functionality might not actually work as of this commit, and I haven't tested it because I don't have the Syntellia TestFlight key, which is required to automatically push the built IPA to TestFlight.  However, these scripts and logic were taken from an older iOS app where all this TestFlight machinery worked.
    
    This commit also adds the script foo needed to add git shorthash tags to the apps version variable.

---
+++ BuildUtil/TestFlightPrebuild.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+#  TestFlightPrebuild.sh
+#  Fleksy
+#
+#  Created by John Engelhart on 3/27/13.
+#  Copyright (c) 2012 Syntellia Inc. All rights reserved.
+
+# This script needs to:
+#
+# Check git to see if there's any modifications that aren't commited, and abort if so.
+# Do the "Edit the release notes" bit
+# Increment TestFlightVersion.xcconfig by one.
+# Commit and push via git the change to TestFlightVersion.xcconfig by one with a message of the release notes.
+#
+# If there is some kind of error, abort and roll back to pre-script run state for all state mutations.
+
+if [ "${CONFIGURATION}" -ne "TestFlight" ]; then
+	exit 0;
+fi
+
+# Check if there's any outstanding modifications that need to be commited, and abort if so.
+#
+# TODO!
+#
+
+
+# Do the "Edit the release notes" bit.
+
+RELEASE_NOTES_FILE=`mktemp /tmp/Release_Notes.XXXXXXXX`
+osascript -e 'tell application "Xcode"' -e 'try' -e "open \"$RELEASE_NOTES_FILE\"" -e 'activate' -e "set okButton to \"OK\"" -e "set rn_dialog to display dialog \"Enter the release notes, then close the window to continue.\" buttons {okButton} default button okButton with icon 1" -e "set rndoc to open \"$RELEASE_NOTES_FILE\"" -e 'repeat while exists rndoc' -e 'delay 1' -e 'end repeat' -e 'save rndoc' -e 'on error return' -e 'end try' -e 'end tell'
+if [ "$?" -ne 0 ]; then
+	exit 1
+fi
+
+
+# Increment TestFlightVersion.xcconfig by one.
+
+if [ -w "${SRCROOT}/${FL_BUILDUTIL_DIR}/TestFlightVersion.xcconfig" ]; then
+	/usr/bin/perl -pi -e 's/(^\s*FL_TESTFLIGHT_BUILD_NUMBER\s*=\s*)(\d+)/$1.($2+1)/eg' "${SRCROOT}/${FL_BUILDUTIL_DIR}/TestFlightVersion.xcconfig"
+	if [ "$?" -ne 0 ]; then
+		exit 1
+	fi
+else
+	exit 1
+fi
+
+cd "${SRCROOT}"
+if [ -r "${SRCROOT}/${FL_BUILDUTIL_DIR}/TestFlightVersion.xcconfig" ]; then
+	git commit -F "${RELEASE_NOTES_FILE}"  "${SRCROOT}/${FL_BUILDUTIL_DIR}/TestFlightVersion.xcconfig"
+#	git commit -F "${RELEASE_NOTES_FILE}" -a
+	if [ "$?" -ne 0 ]; then
+		git checkout  "${SRCROOT}/${FL_BUILDUTIL_DIR}/TestFlightVersion.xcconfig"
+		exit 1
+	fi
+else
+	exit 1
+fi
+
