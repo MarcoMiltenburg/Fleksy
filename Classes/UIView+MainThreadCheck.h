c34b03a6c2e7f7356ed082e3251a76e4f594ecb0
Vince Mansel <vince@syntellia.com>
2013-04-03 09:07:43 -0700

    Added a utility category on UIView to insure that objects are instantiated on main thread.

---
+++ Classes/UIView+MainThreadCheck.h
@@ -0,0 +1,13 @@
+//
+//  UIView+MainThreadCheck.h
+//  iFleksy
+//
+//  Created by Vince Mansel on 4/2/13.
+//  Copyright (c) 2013 Syntellia Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (MainThreadCheck)
+
+@end
