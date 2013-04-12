f6e9a2414f9f84c7bec8df69c05544d3cc708ffc
Vince Mansel <vince@syntellia.com>
2013-04-11 17:02:16 -0700

    Experimental "vertical suggestions on cursor" > FLSuggestionsView:76 from self.vertical = YES;

--- Classes/FLSuggestionsView.mm
+++ Classes/FLSuggestionsView.mm
@@ -73,7 +73,7 @@
   if ( (self = [super init]) ) {
     // Initialization code
     
-    self.vertical = NO;
+    self.vertical = YES;
 
     self->listener = _listener;
     self->capitalization = nil;
