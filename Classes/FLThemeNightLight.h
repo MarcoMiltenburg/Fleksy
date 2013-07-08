e4a21c4382143eb62b0ed03928c9bace4ddacb11
Vince Mansel <vince@syntellia.com>
2013-07-08 13:30:05 -0700

    [Delivers #53007765] 3 Themes for 1.8 Release, DarkSide, Vanilla, Contrast

--- Classes/FLThemeNightLight.h
+++ Classes/FLThemeNightLight.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 Syntellia Inc. All rights reserved.
 //
 
+// Derived Class - Night Light, otherwise known as Contrast
+
 #import "FLTheme.h"
 
 @interface FLThemeNightLight : FLTheme
